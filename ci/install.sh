df415b2ed9910c6eb9a76a04cc48db2077c39104
Camillo Dell'mour <cdellmour@gmail.com>
2022-02-23 21:27:58 +0100

    made scripts executable

--- ci/install.sh
+++ ci/install.sh
@@ -1,14 +1,8 @@
 set -ex
 
 main() {
-    local target=
-    if [ $TRAVIS_OS_NAME = linux ]; then
-        target=x86_64-unknown-linux-musl
-        sort=sort
-    else
-        target=x86_64-apple-darwin
-        sort=gsort  # for `sort --sort-version`, from brew's coreutils.
-    fi
+    target=x86_64-unknown-linux-musl
+    sort=sort
 
     # Builds for iOS are done on OSX, but require the specific target to be
     # installed.
