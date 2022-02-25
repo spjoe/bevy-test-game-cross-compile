1becaf6fc41382fe0e681583898145986034678a
Camillo Dell'mour <cdellmour@gmail.com>
2022-02-25 15:52:30 +0100

    travis ci try.

--- ci/script.sh
+++ ci/script.sh
@@ -4,15 +4,18 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    /root/.cargo/bin/cross build --target $TARGET
-    /root/.cargo/bin/cross build --target $TARGET --release
+    cross build --target $TARGET
+    cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    /root/.cargo/bin/cross test --target $TARGET
-    /root/.cargo/bin/cross test --target $TARGET --release
+    cross test --target $TARGET
+    cross test --target $TARGET --release
+
+    cross run --target $TARGET
+    cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
